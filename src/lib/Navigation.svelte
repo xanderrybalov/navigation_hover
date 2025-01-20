71e1e592b9593882d97a46a4ad509c5287b529e9
Oleksandr Rybalov <xanderrybalov@gmail.com>
2025-01-21 00:06:24 +0200

    fixed: fixed navigation

--- src/lib/Navigation.svelte
+++ src/lib/Navigation.svelte
@@ -1,34 +1,32 @@
 <script>
-    export let active = "Branding";
-    const links = ["Animation", "Branding", "Illustration"];
-  </script>
-  
-  <nav aria-label="Main Navigation" class="flex justify-center items-center h-screen bg-gradient-to-b from-blue-900 to-black">
-    <ul class="flex space-x-10 text-lg font-semibold text-gray-400">
-      {#each links as link}
-        <li class="relative">
-          <button
-            class="cursor-pointer hover:text-white transition-colors duration-300 focus:outline-none"
-            on:click={() => (active = link)}
-            on:keydown={(e) => e.key === 'Enter' && (active = link)}
-            aria-current={active === link ? 'page' : undefined}
-          >
-            <span class="{active === link ? 'text-white' : ''}">{link}</span>
-            {#if active === link}
-              <div
-                class="absolute left-0 right-0 bottom-[-4px] h-[2px] bg-white transition-transform duration-300 scale-x-100"
-              ></div>
-            {/if}
-          </button>
-        </li>
-      {/each}
-    </ul>
-  </nav>
+  export let active = "Branding";
+  const links = ["Animation", "Branding", "Illustration"];
+</script>
 
-  <style>
-    nav ul li button {
-      padding-bottom: 0.5rem;
-    }
-    </style>
-  
-  
+<nav aria-label="Main Navigation" class="flex justify-center items-center w-full bg-transparent py-4">
+  <ul class="flex space-x-10 text-lg font-semibold text-gray-400">
+    {#each links as link}
+      <li class="relative">
+        <button
+          class="cursor-pointer hover:text-white transition-colors duration-300 focus:outline-none"
+          on:click={() => {
+            console.log('Previous active:', active);
+            active = link;
+            console.log('New active:', active);
+          }}
+          on:keydown={(e) => e.key === 'Enter' && (active = link)}
+          aria-current={active === link ? 'page' : undefined}
+        >
+          <span class="{active === link ? 'text-white' : ''}">{link}</span>
+          {#if active === link}
+          <div 
+          class="absolute left-0 right-0 top-11 h-0.5 bg-white transform scale-x-0 transition-transform ease-in-out duration-1000 origin-center"
+          class:scale-x-100={active === link}
+        ></div>
+          {/if}
+        </button>
+      </li>
+    {/each}
+  </ul>
+</nav>
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
     import Navigation from '$lib/Navigation.svelte';
   </script>
   
-  <div class="min-h-screen bg-gradient-to-b from-blue-900 to-black flex items-center justify-center">
+  <div class="min-h-screen bg-gradient-to-b from-black to-blue-900 flex items-center justify-center">
     <Navigation />
   </div>
   
