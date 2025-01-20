7380dc1571736658e58935ae0446fbb275953cb9
Oleksandr Rybalov <xanderrybalov@gmail.com>
2025-01-21 00:08:31 +0200

    fix: fixed animation underline

--- src/lib/Navigation.svelte
+++ src/lib/Navigation.svelte
@@ -9,21 +9,15 @@
       <li class="relative">
         <button
           class="cursor-pointer hover:text-white transition-colors duration-300 focus:outline-none"
-          on:click={() => {
-            console.log('Previous active:', active);
-            active = link;
-            console.log('New active:', active);
-          }}
+          on:click={() => (active = link)}
           on:keydown={(e) => e.key === 'Enter' && (active = link)}
           aria-current={active === link ? 'page' : undefined}
         >
           <span class="{active === link ? 'text-white' : ''}">{link}</span>
-          {#if active === link}
           <div 
-          class="absolute left-0 right-0 top-11 h-0.5 bg-white transform scale-x-0 transition-transform ease-in-out duration-1000 origin-center"
-          class:scale-x-100={active === link}
-        ></div>
-          {/if}
+            class="absolute left-0 right-0 top-11 h-0.5 bg-white transition-all duration-300 ease-in-out"
+            style="transform: scaleX({active === link ? 1 : 0})"
+          ></div>
         </button>
       </li>
     {/each}
