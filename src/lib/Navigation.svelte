5b44df674f3167e1f20323745de829e9dd69a366
Oleksandr Rybalov <xanderrybalov@gmail.com>
2025-01-25 20:06:26 +0200

    feat: added preprocessors

--- src/lib/Navigation.svelte
+++ src/lib/Navigation.svelte
@@ -1,29 +1,24 @@
-<script>
+<script lang="ts">
+import NavButton from './NavButton.svelte';
 export let active = "Branding";
 const links = ["Animation", "Branding", "Illustration"];
+
+const handleActivate = (link: string): void => {
+    active = link;
+    console.log(`Active link changed to: ${link}`);
+};
 </script>
 
 <nav aria-label="Main Navigation" class="flex justify-center items-center w-full bg-transparent py-4">
     <ul class="flex space-x-10 text-lg font-semibold text-gray-400">
-        {#each links as link}
-        <li class="relative">
-            <button
-                class="cursor-pointer hover:text-white transition-colors duration-300 focus:text-white focus:outline-none"
-                tabindex="0"
-                on:click={() => {
-                active = link;
-                console.log(`Active link changed to: ${link}`);
-                }}
-                aria-current={active === link ? 'page' : undefined}
-                aria-label={`Go to ${link} section`}
-                >
-                <span class="{active === link ? 'text-white' : ''}">{link}</span>
-                <div
-                    class="absolute left-5 right-5 top-11 h-0.5 bg-white rounded transition-all duration-300 ease-in-out"
-                    style="transform: scaleX({active === link ? 1 : 0})"
-                    ></div>
-            </button>
-        </li>
-        {/each}
-    </ul>
-</nav>
+                {#each links as link}
+                <li class="relative">
+                    <NavButton
+                        {link}
+                        isActive={active === link}
+                        onActivate={() => handleActivate(link)}
+                    />
+                </li>
+                {/each}
+            </ul>
+        </nav>
